048438b0ca5720960801bc0d4b4ceb3dce73bfe3
Дмитрий Храмов <thedcm@yandex.com>
2018-09-04 22:48:36 +0300

    Добавьте файлы проекта.

---
+++ transkart-captcha-noise-remover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace transkart_captcha_noise_remover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
